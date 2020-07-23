e6b4738186e1f39a75b3dadb3eb9aa18b16a7242
bluecmd <bluecmd@google.com>
2020-07-23 02:55:40 -0700

    Add workaround for glib to work on Kokoro
    PiperOrigin-RevId: 322751327

--- kokoro/rodete/kokoro_build.sh
+++ kokoro/rodete/kokoro_build.sh
@@ -5,6 +5,12 @@ cd "${KOKORO_ARTIFACTS_DIR}"/piper/google3/third_party/glome
 
 sudo ./kokoro/rodete/fetch_dependencies.sh
 
+# Internal Google builds requires the glib.h file to be present at
+# third_party/glib/glib/glib.h
+GLIB_DIR=$(dirname $(dpkg -S '*/glib.h' | awk '{print $2; exit}'))
+mkdir -p third_party/glib/
+ln -sf "${GLIB_DIR}" third_party/glib/glib
+
 meson build
 ninja -C build
 meson test -C build
