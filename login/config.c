87cce74716d600d9846314946185550b012a7d8f
Valentin Vidic <vvidic@google.com>
2022-02-27 09:56:35 +0100

    Use OpenSSL INI parser to load configuration

--- login/config.c
+++ login/config.c
@@ -12,94 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <alloca.h>
-#include <ctype.h>
 #include <errno.h>
-#include <stdlib.h>
+#include <openssl/conf.h>
 #include <string.h>
 
 #include "ui.h"
-
-static bool is_empty(const char *line) {
-  for (; isspace(*line); line++) {
-  }
-  return *line == '\0';
-}
-
-static bool is_comment(const char *line) {
-  return line[0] == '#' || line[0] == ';';
-}
-
-static bool is_section(const char *line) { return line[0] == '['; }
-
-static bool is_name(const char *name) {
-  const char *p;
-  for (p = name; isalnum(*p) || *p == '_' || *p == '-'; p++) {
-  }
-  return *p == '\0' && p - name > 0;
-}
-
-static char *section_name(char *line) {
-  char *p;
-  for (p = line + 1; *p != ']' && *p != '\0'; p++) {
-  }
-  if (*p != ']') {
-    return NULL;
-  }
-  char *end = p;
-  if (!is_empty(p + 1)) {
-    return NULL;
-  }
-  *end = '\0';
-  if (is_name(line + 1)) {
-    return line + 1;
-  }
-  return NULL;
-}
-
-static void key_value(char *line, char **key, char **val) {
-  *key = NULL;
-  *val = NULL;
-
-  char *p;
-  for (p = line; !isspace(*p) && *p != '=' && *p != '\0'; p++) {
-  }
-  if (*p == '\0') {
-    return;
-  }
-  char *end = p;
-
-  for (; isspace(*p); p++) {
-  }
-  if (*p != '=') {
-    return;
-  }
-  for (p++; isspace(*p); p++) {
-  }
-  if (*p == '\0') {
-    return;
-  }
-
-  *end = '\0';
-  if (!is_name(line)) {
-    return;
-  }
-
-  char *v = p;
-  for (; !isspace(*p) && *p != '\0'; p++) {
-  }
-  if (*p != '\0') {
-    *p = '\0';
-    for (p++; isspace(*p); p++) {
-    }
-  }
-  if (*p != '\0') {
-    return;
-  }
-
-  *key = line;
-  *val = v;
-}
 
 static bool assign(glome_login_config_t *config, const char *section,
                    const char *key, const char *val) {
@@ -148,57 +65,43 @@ int glome_login_parse_config_file(glome_login_config_t *config) {
     config->config_path = DEFAULT_CONFIG_FILE;
   }
 
-  FILE *f = fopen(config->config_path, "r");
-  if (f == NULL) {
-    if (!required) {
-      return 0;
-    }
-    errorf("ERROR: config file could not be opened: %s\n", strerror(errno));
-    return -1;
+  int rc = -1;
+  CONF *conf = NULL;
+  if ((conf = NCONF_new(NULL)) == NULL) {
+    errorf("ERROR: config file could not be initialized\n");
+    goto out;
   }
 
-  char *section = NULL;
-  char *key, *val;
-  char *line = NULL;
-  size_t len = 0;
-  size_t lines = 0;
-  int rc = -2;
-  while (getline(&line, &len, f) != -1) {
-    lines++;
-    if (is_empty(line) || is_comment(line)) {
-      continue;
-    } else if (is_section(line)) {
-      char *s = section_name(line);
-      if (s == NULL) {
-        errorf(
-            "ERROR: config file parsing failed in line %ld (bad section "
-            "name)\n",
-            lines);
-        goto out;
+  long errline = -1;
+  if (NCONF_load(conf, config->config_path, &errline) <= 0) {
+    if (errline <= 0) {
+      if (required) {
+        errorf("ERROR: config file could not be read\n");
+      } else {
+        rc = 0;
       }
-      free(section);
-      section = strdup(s);
     } else {
-      key_value(line, &key, &val);
-      if (key == NULL || val == NULL) {
-        errorf(
-            "ERROR: config file parsing failed in line %ld (bad key/value)\n",
-            lines);
-        goto out;
-      }
-      if (!assign(config, section, key, val)) {
-        errorf(
-            "ERROR: config file parsing failed in line %ld (bad assignment)\n",
-            lines);
-        goto out;
-      }
+      errorf("ERROR: config file could not be parsed (line %ld)\n", errline);
+    }
+    goto out;
+  }
+
+  STACK_OF(CONF_VALUE) *sect = NCONF_get_section(conf, "service");
+  if (sect == NULL) {
+    rc = 0;
+    goto out;
+  }
+
+  for (int i = 0; i < sk_CONF_VALUE_num(sect); i++) {
+    CONF_VALUE *v = sk_CONF_VALUE_value(sect, i);
+    if (!assign(config, v->section, v->name, v->value)) {
+      rc = -2;
+      goto out;
     }
   }
   rc = 0;
 
 out:
-  free(line);
-  free(section);
-  fclose(f);
+  NCONF_free(conf);
   return rc;
 }
